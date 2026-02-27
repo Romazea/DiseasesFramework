41232e429a079ea961456e2d4b7c058990f198fa
Leve <levelith0@gmail.com>
2026-02-27 09:57:47 -0400

    First commit

---
+++ Source/DiseasesFramework/DiseasesFrameworkCore.cs
@@ -0,0 +1,12 @@
+﻿using Verse;
+
+namespace DF
+{
+    public class DiseasesFrameworkCore : Mod
+    {
+        public DiseasesFrameworkCore(ModContentPack content) : base(content)
+        {
+            Log.Message("<color=green>[Disease Framework] Successfully loaded</color>");
+        }
+    }
+}

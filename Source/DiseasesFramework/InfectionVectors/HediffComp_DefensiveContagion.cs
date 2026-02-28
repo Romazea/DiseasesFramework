f211761b994cd1f8535747e9e6dcf9ff6be0f409
Leve <levelith0@gmail.com>
2026-02-28 11:22:45 -0400

    Added armor penetration and toxic resistance mechanics to DefensiveContagion

--- Source/DiseasesFramework/InfectionVectors/HediffComp_DefensiveContagion.cs
+++ Source/DiseasesFramework/InfectionVectors/HediffComp_DefensiveContagion.cs
@@ -1,18 +1,22 @@
 ﻿using Verse;
 using RimWorld;
+using UnityEngine;
 
 namespace DiseasesFramework.InfectionVectors
 {
-    public class HediffCompProperties_CombatContagion : HediffCompProperties
+    public class HediffCompProperties_DefensiveContagion : HediffCompProperties
     {
         public float infectionChance = 0.3f;
         public HediffDef hediffToApply;
         public bool onlyMelee = true;
 
+        public bool requiresDamageToPenetrate = true;
+        public bool respectsToxicResistance = false;
+
         public bool sendNotification = false;
         public bool useLetterInsteadOfMessage = false;
 
-        public HediffCompProperties_CombatContagion()
+        public HediffCompProperties_DefensiveContagion()
         {
             this.compClass = typeof(HediffComp_DefensiveContagion);
         }
@@ -20,7 +24,7 @@ namespace DiseasesFramework.InfectionVectors
 
     public class HediffComp_DefensiveContagion : HediffComp
     {
-        public HediffCompProperties_CombatContagion Props => (HediffCompProperties_CombatContagion)this.props;
+        public HediffCompProperties_DefensiveContagion Props => (HediffCompProperties_DefensiveContagion)this.props;
 
         public override void Notify_PawnPostApplyDamage(DamageInfo dinfo, float totalDamageDealt)
         {
@@ -33,7 +37,12 @@ namespace DiseasesFramework.InfectionVectors
 
             if (Props.hediffToApply == null)
             {
-                Log.ErrorOnce("[Disease Framework] An XML is trying to use CombatContagion but is missing the <hediffToApply> tag.", this.Pawn.thingIDNumber ^ 84729);
+                Log.ErrorOnce("[Disease Framework] An XML is trying to use DefensiveContagion but is missing the <hediffToApply> tag.", this.Pawn.thingIDNumber ^ 84729);
+                return;
+            }
+
+            if (Props.requiresDamageToPenetrate && totalDamageDealt <= 0f)
+            {
                 return;
             }
 
@@ -56,7 +65,20 @@ namespace DiseasesFramework.InfectionVectors
                     return;
                 }
 
-                if (Rand.Chance(Props.infectionChance))
+                float finalChance = Props.infectionChance;
+
+                if (Props.respectsToxicResistance)
+                {
+                    float bioRes = attacker.GetStatValue(StatDefOf.ToxicResistance);
+
+                    float envRes = attacker.GetStatValue(StatDefOf.ToxicEnvironmentResistance);
+
+                    float bestProtection = Mathf.Max(bioRes, envRes);
+
+                    finalChance *= Mathf.Clamp01(1f - bestProtection);
+                }
+
+                if (Rand.Chance(finalChance))
                 {
                     attacker.health.AddHediff(Props.hediffToApply);
 

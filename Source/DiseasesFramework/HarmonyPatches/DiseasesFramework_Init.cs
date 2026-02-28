dcb730f706fe68170e322efdf3aefff5ef0ac5e1
Leve <levelith0@gmail.com>
2026-02-28 09:44:50 -0400

    Integrated Harmony library and updated mod dependencies

---
+++ Source/DiseasesFramework/HarmonyPatches/DiseasesFramework_Init.cs
@@ -0,0 +1,18 @@
+﻿using Verse;
+using HarmonyLib;
+
+namespace DiseasesFramework
+{
+    [StaticConstructorOnStartup]
+    public static class DiseasesFramework_Init
+    {
+        static DiseasesFramework_Init()
+        {
+            Harmony harmony = new Harmony("com.romazea.diseasesframework");
+
+            harmony.PatchAll();
+
+            Log.Message("<color=cyan>[Disease Framework] Harmony patches applied successfully.</color>");
+        }
+    }
+}
